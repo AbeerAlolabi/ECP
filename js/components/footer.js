f9024138ad58e848233ba7034a1b8f7cadd258a8
Abeer_Alolabi <a.alolabi@hotmail.com>
2021-03-10 18:37:00 +0200

    second edit

---
+++ js/components/footer.js
@@ -0,0 +1,94 @@
+class Footer extends HTMLElement {
+    constructor(){
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML =`
+            <style>
+            /* footer styling */
+
+            footer{
+                background-color:  rgb(6,78,74) ;
+                margin-bottom: 0px;
+                padding: 25px 0px;
+                display: flex;
+            }
+            
+            footer h3 {
+                color:rgb(255,254,246);
+                font-size: 26px ;
+            }
+            
+            footer p{
+                color: rgb(151, 155, 152);
+            }
+            
+            
+            
+            .related_sites{
+                color: rgb(151, 155, 152);
+                text-decoration: underline;
+            }
+            
+            .related_sites:hover{
+                color: rgb(255,139,0);
+            }
+            
+            .material-icons{
+               color: rgb(255,139,0);
+               border: 1px solid rgba(244, 250, 246,0.8);
+               padding: 3px;
+               border-radius: 50%;
+               font-size: 20px;
+               align-items: flex-endS;
+            }
+            
+            .material-icons:hover{
+                border: 1px solid rgb(255,139,0);
+             }
+            
+            .left_footer p{
+                margin-left: 40px;
+            }
+            
+            .middle_footer p{
+                margin-left: 44px;
+            }
+            
+            .right_footer p{
+                margin-left: 43px;
+            }
+            </style>
+
+            <footer>
+            <div class="container">
+            <div class="row justify-content-center">
+                
+                <div class="col-12 col-md-4 left_footer ">
+                    <h3><i class="material-icons">location_on</i> Address</h3> 
+                    <p>
+                    University of Toronto<br>
+                    Ontario Institute for Studies in Education<br>
+                    252 Bloor Street West<br>
+                    Toronto, ON M5S 1V6<br>
+                    </p>
+                </div>
+                
+                <div class="col-12 col-md-4 col-lg-3  middle_footer"> 
+                <h3> <i class="material-icons">&#xe7fb;</i> Related site</h3>
+                <p><a href="https://www.oise.utoronto.ca/cidec/" class="related_sites">www.oise.utoronto.ca</a></p>
+                </div>
+        
+                <div class="col-12 col-md-4  right_footer ">
+                    <h3><i class="material-icons">&#xe159;</i> Email</h3>
+                    <p>ecp.oise@gmail.com </p>
+                </div>
+        
+            </div> 
+            </div>
+        </footer>
+        `;
+    }
+}
+
+customElements.define('footer-component', Footer);
---
+++ js/components/navbar.js
@@ -0,0 +1,98 @@
+class Navbar extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        <style>
+                
+          .navbar-brand{
+            padding: 0px 20px;
+          }
+  
+          .navbar-nav{
+            padding: 25px 0px 0px 50px;
+          
+          }
+  
+          .navbar{
+            background-color: rgb(3,153,144);
+          }
+  
+          .nav-link{
+            color: rgb(244, 250, 246);
+            font-size: large;
+            font-weight: bold;
+            margin: 0px 15px;
+          }
+  
+          .nav-link:hover{
+            color: rgb(255,139,0);
+          }
+  
+          .dropdown-menu{
+            margin: 0px 15px;
+            padding-bottom: 0px;
+            background-color: rgb(3,153,144);
+            
+          }
+  
+          .dropdown-menu > li{
+            padding: 2px 10px;
+            border-bottom: 2px solid rgba(0, 63,63,0.2);
+          }
+  
+          .dropdown-menu > li:hover{
+            background-color: rgb(255,139,0);
+          }
+  
+          .dropdown-menu > li > a{
+            color: rgb(244, 250, 246);
+          }
+  
+          .dropdown-menu > li > a:hover{
+  
+          text-decoration: none;
+          }
+  
+          .navbar-toggler{
+            border: 1px solid rgba(244, 250, 246,0.8);
+            background-color: rgba(55, 55, 55,0.1);
+          }
+  
+          /* end of nav bar style */
+  
+        </style>
+  
+        
+        <nav class="navbar navbar-expand-lg  ">
+          <div class="container align-items-end" justify-content-center>
+            <div class="navbar-header">
+              <a class="navbar-brand" href="index.html"><img width= "100px" src="images/logo.png" alt="logo"></a>
+            </div>
+  
+            <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+              <span class="navbar-toggler-icon "></span>
+            </button>
+            
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+              <ul class="nav navbar-nav ">
+                <li class="active nav-item"><a href="index.html" class="nav-link ">Home</a></li>
+                <li class="dropdown nav-item"><a class="dropdown-toggle nav-link" data-toggle="dropdown" href="#">About<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#" class="">About us</a></li>
+                    <li><a href="#">Our Members</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item"><a href="blogs.html" class="nav-link">Blogs</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Events</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Contact us</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        `;  
+    }
+  }
+      customElements.define('navbar-component', Navbar);
